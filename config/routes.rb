13b24c2e981e433a1a5de011d35f5572bbb95088
Justin Carrasquillo <jcarrasq@uncc.edu>
2020-03-18 00:27:58 +0000

    Set welcome page as root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+  
+  #sets the welcome page as the default view
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
