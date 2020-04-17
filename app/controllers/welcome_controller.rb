8355bd872fecef0b169ce3e8f69c803c5cb6b84c
EC2 Default User <ec2-user@ip-172-31-45-119.ec2.internal>
2020-04-17 17:55:17 +0000

    html icon can change dpending on the bed.length variable

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ApplicationController
   def index
     @beds = Bed.all
+    @beginImageHTML = '<img src="'
+    @endImageHTMLYES ='" ' + 'class="indicate-icon" alt="available bed icon">'
+    @endImageHTMLNO ='" ' + 'class="indicate-icon" alt="no available bed icon">'
   end
 end
