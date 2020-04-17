86029518a5aab7141a7d52b5693f65ee9f64e727
EC2 Default User <ec2-user@ip-172-31-45-119.ec2.internal>
2020-04-17 18:09:24 +0000

    got html tag: main to work with bed length variable

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,5 +4,7 @@ class WelcomeController < ApplicationController
     @beginImageHTML = '<img src="'
     @endImageHTMLYES ='" ' + 'class="indicate-icon" alt="available bed icon">'
     @endImageHTMLNO ='" ' + 'class="indicate-icon" alt="no available bed icon">'
+    @redMain = '<main class="main red" id="main-id">'
+    @greenMain = '<main class="main green" id="main-id">'
   end
 end
