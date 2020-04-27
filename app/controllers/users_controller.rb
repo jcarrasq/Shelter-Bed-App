4ddcaf5e8ae8c0bd25bf2621cf2e2753d182661f
Ubuntu <ubuntu@ip-172-31-38-28.ec2.internal>
2020-04-27 02:10:24 +0000

    Admin on user edit and destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-
+  
   # GET /users
   # GET /users.json
   def index
@@ -25,12 +25,11 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
+    
     respond_to do |format|
       if @user.save
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
-        
       else
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
